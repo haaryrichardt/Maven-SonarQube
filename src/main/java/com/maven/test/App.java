d7a6f71e69e17d00c1a9ada62cabe460b02f9298
Krishna Srinivas <32317345+Krishna-Srinivas6987@users.noreply.github.com>
2019-10-03 04:34:28 -0700

    Update App.java

--- src/main/java/com/maven/test/App.java
+++ src/main/java/com/maven/test/App.java
@@ -6,8 +6,12 @@ package com.maven.test;
  */
 public class App 
 {
+    public int add(int a, int b){
+        return a+b;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App ap = new App();
+        int sum = ap.add(2,3);
     }
 }
